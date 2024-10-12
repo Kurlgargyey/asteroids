2498765cea4e4e214263347059a016e277b21121
Kurlgargyey <nahsirkivar@gmail.com>
2024-10-12 16:46:28 +0200

    switch rendering implementation to use sprite groups

--- main.py
+++ main.py
@@ -8,8 +8,13 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
     game_clock = pygame.time.Clock()
     dt = 0
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
 
     player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
     while True:
@@ -17,8 +22,10 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
-        player.update(dt)
-        player.draw(screen)
+        for object in updatable:
+            object.update(dt)
+        for object in drawable:
+            object.draw(screen)
         pygame.display.flip()
         dt = game_clock.tick(60)
 
